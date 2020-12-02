b87cf75d948147db8a24ab50aba7563b4c087f1f
flora <=>
2020-12-02 13:17:52 +0100

    Day 2 - part 2

--- day2.hs
+++ day2.hs
@@ -1,6 +1,7 @@
 import System.IO
 import Data.List.Split
 
+-- PART 1
 numberOfOccurences :: String -> Char -> Int
 numberOfOccurences password letter = length $ filter (==letter) password
 
@@ -20,8 +21,32 @@ evaluatePassword line =
     letter = (line !! 2) !! 0
     password = line !! 3
 
+-- PART 2
+inCorrectPosition :: String -> Char -> Int -> Int
+inCorrectPosition password letter position =
+  if password !! (position - 1) == letter
+    then 1
+    else 0
+
+numberOfCorrectPositions :: String -> Char -> Int -> Int -> Int
+numberOfCorrectPositions password letter pos1 pos2 = (inCorrectPosition password letter pos1) + (inCorrectPosition password letter pos2)
+
+evaluatePassword2 :: [String] -> Bool
+evaluatePassword2 line =
+  (numberOfCorrectPositions password letter pos1 pos2) == 1
+  where
+    pos1 = read (line !! 0) :: Int
+    pos2 = read (line !! 1) :: Int
+    letter = (line !! 2) !! 0
+    password = line !! 3
+
 main :: IO ()
 main = do
   infile <- readFile "day2_input.txt"
-  let evaluatedPasswords = [ evaluatePassword (splitOneOf " -" x) | x <- lines infile]
+  let list = [ splitOneOf " -" x | x <- lines infile]
+
+  let evaluatedPasswords = [ evaluatePassword x | x <- list]
   print $ length $ filter (==True) evaluatedPasswords
+
+  let evaluatedPasswords2 = [ evaluatePassword2 x | x <- list]
+  print $ length $ filter (==True) evaluatedPasswords2
