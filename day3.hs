49c6c9c0c8965646b35c34c06b51e42f7bc904cf
flora <=>
2020-12-03 22:29:51 +0100

    Day 3 - part 2

--- day3.hs
+++ day3.hs
@@ -1,10 +1,18 @@
 import System.IO
 
-getCol :: Int -> Int -> Int
-getCol lineLength col =
-  if col + 3 >= lineLength
-    then col + 3 - lineLength
-    else col + 3
+colSlopes :: [Int]
+colSlopes = [1, 3, 5, 7, 1]
+
+lineSlopes :: [Int]
+lineSlopes = [1, 1, 1, 1, 2]
+
+data Slope = Slope {
+  count :: Int,
+  lineShift :: Int,
+  colShift :: Int,
+  lineIndex :: Int,
+  colIndex :: Int
+} deriving (Show)
 
 addTree :: [String] -> Int -> Int -> Int
 addTree list line col =
@@ -12,18 +20,46 @@ addTree list line col =
     then 1
     else 0
 
-countTrees :: [String] -> Int -> Int -> Int -> Int
-countTrees list count line col =
-  if line >= (length list)
-    then count
-    else countTrees list new_count new_line new_col
+getCol :: Int -> Slope -> Int
+getCol lineLength slope =
+  if col + slopeShift >= lineLength
+    then col + slopeShift - lineLength
+    else col + slopeShift
+  where
+    slopeShift = colShift slope
+    col = colIndex slope
+
+getSlopeLine :: Slope -> Int
+getSlopeLine slope =
+  (lineIndex slope) + (lineShift slope)
+
+
+updateSlope :: [String] -> Slope -> Int -> Slope
+updateSlope list slope currentIndex =
+  if currentIndex == lineIndex slope
+    then Slope { count = count slope + (addTree list currentIndex (colIndex slope)), lineShift = lineShift slope, colShift = colShift slope, lineIndex = getSlopeLine slope, colIndex = getCol lineLength slope  }
+    else slope
     where
-      new_count = count + addTree list line col
-      new_line = line + 1
-      new_col = getCol (length(list !! line)) col
+      lineLength =  length $ list !! currentIndex
+
+countTrees :: [String] -> [Slope] -> Int -> [Int]
+countTrees list slopes index =
+  if index == (length list)
+    then [ count slope | slope <- slopes]
+    else countTrees list [ updateSlope list slope index | slope <- slopes] (index + 1)
+
 
 main :: IO ()
 main = do
   infile <- readFile "day3_input.txt"
   let list = [ x | x <- lines infile]
-  print(countTrees list 0 0 0)
+
+  let slope1 = Slope { count = 0, lineShift = 1, colShift = 1, lineIndex = 0, colIndex = 0 }
+  let slope2 = Slope { count = 0, lineShift = 1, colShift = 3, lineIndex = 0, colIndex = 0 }
+  let slope3 = Slope { count = 0, lineShift = 1, colShift = 5, lineIndex = 0, colIndex = 0 }
+  let slope4 = Slope { count = 0, lineShift = 1, colShift = 7, lineIndex = 0, colIndex = 0 }
+  let slope5 = Slope { count = 0, lineShift = 2, colShift = 1, lineIndex = 0, colIndex = 0 }
+
+  let slopes = [slope1, slope2, slope3, slope4, slope5]
+  let counts = countTrees list slopes 0
+  print $  foldl (*) 1 counts
